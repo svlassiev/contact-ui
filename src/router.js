f6d6dd9d5af142d06b2a47d1047b763378e1828b
Sergey <sergei.vlasev@pingpong.se>
2019-09-13 16:20:49 +0200

    Add custom nginx.conf, add router

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      name: 'app',
+      component: App
+    },
+    {
+      path: '*',
+      redirect: { name: 'app' }
+    }
+  ]
+})
+
+export default router
